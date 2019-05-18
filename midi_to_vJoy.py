b7686081ce1adbb7bcfd14ce5ee1d7016adbc015
chrislo27 <kinchrislo@gmail.com>
2019-05-18 10:59:58 -0700

    Fix a thing in midi to vJoy

--- midi_to_vJoy.py
+++ midi_to_vJoy.py
@@ -9,7 +9,7 @@ def update():
 	diagnostics.watch(controlNumber)
 	diagnostics.watch(value)
 	if data.status == MidiStatus.Control and controlNumber == 64:
-		lastPedal = filters.mapRange(data.buffer[1], 0, 127, 0, 2**15)
+		lastPedal = filters.mapRange(value, 0, 127, 0, 2**15)
 	vJoy[2].x = lastPedal
 	
 if starting:
